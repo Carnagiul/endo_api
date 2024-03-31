402e850510e98e32d18b222d77c6c20b8877e81b
Carnagiul <pierrequeruel@gmail.com>
2024-03-31 16:21:54 +0200

    fix api

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,7 @@ Route::get('/user', function (Request $request) {
 
 Route::group([
     'as' => 'api.',
-    // 'middleware' => ['auth:sanctum'],
+    'middleware' => ['auth:sanctum'],
 ], function() {
     Route::group([
         'prefix' => 'players',
@@ -67,7 +67,7 @@ Route::group([
         "prefix" => "translations",
         "as" => "translations." 
     ], function() {
-        Route::get("list", [TranslationsAPI::class, 'list'])->name('list');
-        // Route::post("list", [TranslationsAPI::class, 'list'])->name('list');
+        // Route::get("list", [TranslationsAPI::class, 'list'])->name('list');
+        Route::post("list", [TranslationsAPI::class, 'list'])->name('list');
     });
 });
